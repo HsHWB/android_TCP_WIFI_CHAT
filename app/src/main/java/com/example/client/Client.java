5553c5836bb4634227cd48ed53b290f31382d9ff
HsHWB <Hswbinforgithub@gmail.com>
2015-05-17 23:52:49 +0800

    #写好客户端

---
+++ app/src/main/java/com/example/client/Client.java
@@ -0,0 +1,97 @@
+package com.example.client;
+
+import com.example.utils.Constant;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+/**
+ * Created by root on 17/05/15.
+ */
+public class Client {
+
+    //服务器端口
+    private static final int SERVICE_PORT = Constant.SERVICE_PORT;
+    private static final String SERVICE_HOST = Constant.SERVICE_HOST;
+    private BufferedReader br;
+    private PrintWriter pw;
+    private Socket socket;
+    private String getMsg;
+    private String pullMsg;
+
+    /**
+     * 传入端口以及客户聊天对应的textview
+     * @param port
+     * @param textView
+     */
+    public Client(String port, TextView textView){
+        //Socket实例化，连接服务器
+        try {
+            socket = new Socket(Constant.SERVICE_HOST, Constant.SERVICE_PORT);
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+        }catch (IOException e){
+            System.out.println("Service ");
+        }
+
+    }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while(true){
+                try{
+                    if ( (getMsg = br.readLine()) != null ){
+                        pullMsg += getMsg;
+                        System.out.println("Client  pullMsg == " + pullMsg);
+                    }else{
+                        break;
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            if (pullMsg != null){
+                Message msg = new Message();
+                msg.what = 0x123;
+                mHandler.sendMessage(msg);
+            }
+        }
+    };
+
+    public void msgToService(String msg){
+        pw.print(msg);
+        pw.flush();//刷新缓冲区马上发送服务端
+    }
+
+
+
+    public int getSERVICE_PORT(){
+        return SERVICE_PORT;
+    }
+
+    public String getServiceHost(){
+        return SERVICE_HOST;
+    }
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+
+            if (msg.what == 0x123){
+
+            }
+
+        }
+    };
+
+}
--- app/src/main/java/com/example/server/service.java
+++ app/src/main/java/com/example/server/service.java
@@ -4,6 +4,8 @@ import com.example.utils.Constant;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,20 +16,6 @@ import java.util.concurrent.ExecutorService;
  * Created by root on 17/05/15.
  */
 public class service {
-    //服务器端口
-    private static int SERVICE_PORT;
-    private BufferedReader br;
-    private PrintWriter pw;
-    private Socket socket;
 
-    public service(String port){
-        //Socket实例化，连接服务器
-        try {
-            socket = new Socket(Constant.SERVICE_HOST, Constant.SERVICE_PORT);
-        }catch (IOException e){
-            System.out.println("Service ");
-        }
-
-    }
 
 }
